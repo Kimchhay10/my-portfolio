85aa247a87d267a85521faf21e902247fb8b14cf
@ethkimchhay03 <ethkimchhay03@gmail.com>
2022-12-11 16:31:59 +0700

    add: initial

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "36b22d6fc5a9ad6e"; }
+/******/ 		__webpack_require__.h = function() { return "2781514d585b6ae7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
